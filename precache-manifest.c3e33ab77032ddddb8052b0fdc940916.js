91e79ddcade558ec9c4f3d5c7fdbb6c96371ad30
DJ Krummen <44108933+krummenCode@users.noreply.github.com>
2019-12-05 08:29:23 -0600

    Updates

---
+++ precache-manifest.c3e33ab77032ddddb8052b0fdc940916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbaea5ae6ece05ec599f20a33db7667",
+    "url": "/React-Movies/index.html"
+  },
+  {
+    "revision": "c7933d1c8ab750aee415",
+    "url": "/React-Movies/static/css/main.2901a7d7.chunk.css"
+  },
+  {
+    "revision": "cbbf25087e9cb7824caa",
+    "url": "/React-Movies/static/js/2.e85dbcb1.chunk.js"
+  },
+  {
+    "revision": "c7933d1c8ab750aee415",
+    "url": "/React-Movies/static/js/main.cd7394dd.chunk.js"
+  },
+  {
+    "revision": "2a82cdac6791bdd903e6",
+    "url": "/React-Movies/static/js/runtime-main.d0a60ce0.js"
+  }
+]);
